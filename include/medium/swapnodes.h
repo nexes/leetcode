0f020a28dd999b1edd89ceb1fe7d7b3ca48e698a
Joe Berria <joeberria@gmail.com>
2020-05-29 14:46:34 -0700

    move listnode object in its own header file

--- include/medium/swapnodes.h
+++ include/medium/swapnodes.h
@@ -1,6 +1,9 @@
 #pragma once
 
-namespace Leet::Medium {
+#include <../listnode.h>
+
+namespace Leet::Medium
+{
 
     // Given a linked list, swap every two adjacent nodes and return its head.
     // You may not modify the values in the list's nodes, only nodes itself may be changed.
@@ -9,33 +12,24 @@ namespace Leet::Medium {
     // Given 1->2->3->4, you should return the list as 2->1->4->3.
 
     // Definition for singly-linked list taken from LeetCode problem
-    struct ListNode
-    {
-        int val;
-        ListNode *next;
-
-        ListNode() : val(0), next(nullptr) {}
-        ListNode(int x) : val(x), next(nullptr) {}
-        ListNode(int x, ListNode *next) : val(x), next(next) {}
-    };
-
     struct SwapNodes
     {
         // swap every pair while maintaining the original list, creating a new swapped list
-        ListNode *swapPairs(ListNode *head)
+        Leet::ListNode *swapPairs(Leet::ListNode *head)
         {
             if (head == nullptr || head->next == nullptr)
                 return head;
 
-            auto *swapped = new ListNode(head->next->val);
-            swapped->next = new ListNode(head->val);
+            auto *swapped = new Leet::ListNode(head->next->val);
+            swapped->next = new Leet::ListNode(head->val);
 
             auto *curr = swapped->next;
             head = head->next->next;
 
-            while (head && head->next) {
-                curr->next = new ListNode(head->next->val);
-                curr->next->next = new ListNode(head->val);
+            while (head && head->next)
+            {
+                curr->next = new Leet::ListNode(head->next->val);
+                curr->next->next = new Leet::ListNode(head->val);
 
                 head = head->next->next;
                 curr = curr->next->next;
@@ -45,11 +39,12 @@ namespace Leet::Medium {
         }
 
         // swap every pair, not maintaining the original list
-        ListNode *swap_pairs_inplace(ListNode *head)
+        Leet::ListNode *swap_pairs_inplace(Leet::ListNode *head)
         {
             auto *orig_head = head;
 
-            while (head && head->next) {
+            while (head && head->next)
+            {
                 std::swap(head->val, head->next->val);
                 head = head->next->next;
             }
@@ -57,4 +52,4 @@ namespace Leet::Medium {
             return orig_head;
         }
     };
-}  // namespace Leet::Medium
+} // namespace Leet::Medium
