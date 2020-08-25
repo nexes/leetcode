3714efa2e9165172db9a31e20e92fa82c34b76e5
Joe Berria <joeberria@gmail.com>
2020-08-25 09:43:00 -0700

    find duplicate number cycle detection medium problem

---
+++ include/medium/duplicate_number.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <vector>
+
+namespace Leet::Medium
+{
+    // Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+    // Example 1:
+    // Input: [1,3,4,2,2]
+    // Output: 2
+
+    // Example 2:
+    // Input: [3,1,3,4,2]
+    // Output: 3
+
+    // Note:
+    // You must not modify the array (assume the array is read only).
+    // You must use only constant, O(1) extra space.
+    // Your runtime complexity should be less than O(n2).
+    // There is only one duplicate number in the array, but it could be repeated more than once.
+    struct DuplicateNumber
+    {
+        // tortoise and the hare
+        // https://en.wikipedia.org/wiki/Cycle_detection
+        int findDuplicate(std::vector<int> &nums)
+        {
+            int tortoise = nums[0];
+            int hare = nums[0];
+
+            do
+            {
+                // per the problem description, nums will contain [1,n] numbers, so we don't need to check bounds
+                tortoise = nums[tortoise];
+                hare = nums[nums[hare]];
+
+            } while (tortoise != hare);
+
+            tortoise = nums[0];
+            while (tortoise != hare)
+            {
+                tortoise = nums[tortoise];
+                hare = nums[hare];
+            }
+
+            return hare;
+        }
+    };
+} // namespace Leet::Medium
