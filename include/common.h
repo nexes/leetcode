49c434f4899c285f51f51ecb1c072e4c3976b610
Joe Berria <joeberria@gmail.com>
2020-08-26 07:58:32 -0700

    adding rotate to common algos, including common tests

--- include/common.h
+++ include/common.h
@@ -1,42 +1,85 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 namespace Leet
 {
-    void ltrim(std::string &s)
+    // Wrapped in a class to avoid duplicate definition errors
+    class Algorithm
     {
-        if (s.empty())
-            return;
-        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](const char &c) { return !std::isspace(c); }));
-    }
+    public:
+        void ltrim(std::string &s)
+        {
+            if (s.empty())
+                return;
+            s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](const char &c) { return !std::isspace(c); }));
+        }
 
-    void rtrim(std::string &s)
-    {
-        if (s.empty())
-            return;
-        // .base() accesses the underlying iterator. needed because this is a reverse iterator
-        s.erase(std::find_if(s.rbegin(), s.rend(), [](const char &c) { return !std::isspace(c); }).base(), s.end());
-    }
+        void rtrim(std::string &s)
+        {
+            if (s.empty())
+                return;
+            // .base() accesses the underlying iterator. needed because this is a reverse iterator
+            s.erase(std::find_if(s.rbegin(), s.rend(), [](const char &c) { return !std::isspace(c); }).base(), s.end());
+        }
 
-    std::vector<std::string> split(std::string &s)
-    {
-        if (s.empty())
-            return {};
-        auto ss = std::istringstream{s};
+        std::vector<std::string> split(std::string &s)
+        {
+            if (s.empty())
+                return {};
+            auto ss = std::istringstream{s};
 
-        // istream_iterator will skip whitespace on its own. We get the functionality for free!
-        return std::vector(std::istream_iterator<std::string>{ss},
-                           std::istream_iterator<std::string>());
-    }
+            // istream_iterator will skip whitespace on its own. We get the functionality for free!
+            return std::vector(std::istream_iterator<std::string>{ss},
+                               std::istream_iterator<std::string>());
+        }
 
-    template <typename T>
-    void reverse(std::vector<T> &v)
-    {
-        if (v.empty())
-            return;
-        auto left = v.begin();
-        auto right = v.end() - 1;
+        template <typename T>
+        void reverse(std::vector<T> &v)
+        {
+            if (v.empty())
+                return;
+            auto left = v.begin();
+            auto right = v.end() - 1;
 
-        for (int i = 0; i < v.size() / 2; i++)
-            std::iter_swap(left++, right--);
-    }
+            for (int i = 0; i < v.size() / 2; i++)
+                std::iter_swap(left++, right--);
+        }
+
+        // rotate in place. time: O(n) space: O(1)
+        template <typename T>
+        void rotate(std::vector<T> &v, int k)
+        {
+            if (v.empty() || k < 1)
+                return;
+
+            auto size = v.size();
+            auto right = size - (k % size);
+
+            // reverse the array from 0 to k
+            for (int i = 0, j = right - 1; i < j; i++, j--)
+            {
+                auto temp = v[i];
+                v[i] = v[j];
+                v[j] = temp;
+            }
+
+            // reverse the array from k to size - 1
+            for (int i = right, j = size - 1; i < j; i++, j--)
+            {
+                auto temp = v[i];
+                v[i] = v[j];
+                v[j] = temp;
+            }
+
+            // reverse the hole list
+            for (int i = 0, j = size - 1; i < j; i++, j--)
+            {
+                auto temp = v[i];
+                v[i] = v[j];
+                v[j] = temp;
+            }
+        }
+    };
 } // namespace Leet
--- test/common_tests.cpp
+++ test/common_tests.cpp
@@ -3,19 +3,23 @@
 
 TEST(Common, string_trim)
 {
+    auto algo = Leet::Algorithm();
+
     std::string s = "     hello !world!";
-    Leet::ltrim(s);
+    algo.ltrim(s);
     ASSERT_EQ(s, "hello !world!");
 
     s = "hello !world! a       ";
-    Leet::rtrim(s);
+    algo.rtrim(s);
     ASSERT_EQ(s, "hello !world! a");
 }
 
 TEST(Common, string_split)
 {
+    auto algo = Leet::Algorithm();
+
     std::string s{"hello world leet code leetcode code what wat"};
-    auto sv = Leet::split(s);
+    auto sv = algo.split(s);
 
     std::vector<std::string> list{"hello", "world", "leet", "code", "leetcode", "code", "what", "wat"};
     ASSERT_EQ(sv, list);
@@ -23,14 +27,34 @@ TEST(Common, string_split)
 
 TEST(Common, reverse_lists)
 {
+    auto algo = Leet::Algorithm();
+
     std::vector<int> nums{1, 2, 3, 4, 5, 6, 7, 8, 9999};
     std::vector<int> rev_nums{9999, 8, 7, 6, 5, 4, 3, 2, 1};
 
-    Leet::reverse(nums);
+    algo.reverse(nums);
     ASSERT_EQ(nums, rev_nums);
 
     std::vector<std::string> words{"the", "bear", "is", "sticky", "from", "the", "honey"};
     std::vector<std::string> words_rev{"honey", "the", "from", "sticky", "is", "bear", "the"};
-    Leet::reverse(words);
+
+    algo.reverse(words);
     ASSERT_EQ(words, words_rev);
+}
+
+TEST(Common, rotate_list)
+{
+    auto algo = Leet::Algorithm();
+
+    std::vector<int> v{1, 2, 3, 4, 5, 6, 7};
+    std::vector<int> rotated{5, 6, 7, 1, 2, 3, 4};
+
+    algo.rotate(v, 3);
+    ASSERT_EQ(v, rotated);
+
+    v = {1, 2, 3, 4, 5, 6, 7};
+    rotated = {7, 1, 2, 3, 4, 5, 6};
+
+    algo.rotate(v, 8);
+    ASSERT_EQ(v, rotated);
 }
