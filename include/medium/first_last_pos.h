657dfcbd0f225514b78a614a77919bc52be50baf
Joe Berria <joeberria@gmail.com>
2020-10-12 23:05:46 -0700

    formatting

--- include/medium/first_last_pos.h
+++ include/medium/first_last_pos.h
@@ -3,8 +3,7 @@
 #include <algorithm>
 #include <iostream>
 
-namespace Leet::Medium
-{
+namespace Leet::Medium {
     // Given an array of integers nums sorted in ascending order, find the starting and
     // ending position of a given target value. Your algorithm's runtime complexity must
     // be in the order of O(log n). If the target is not found in the array, return [-1, -1].
@@ -78,4 +77,4 @@ namespace Leet::Medium
             return found;
         }
     };
-} // namespace Leet::Medium
+}  // namespace Leet::Medium
