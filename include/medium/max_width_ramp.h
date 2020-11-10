968bae81c174e7c5b5d7a408609d04e304f70300
Joe Berria <joeberria@gmail.com>
2020-11-10 11:49:23 -0800

    adding notes

--- include/medium/max_width_ramp.h
+++ include/medium/max_width_ramp.h
@@ -26,6 +26,14 @@ namespace Leet::Medium {
     // 0 <= A[i] <= 50000
     struct MaxWidthRamp
     {
+        // a = [6, 0, 8, 2, 1, 5]
+        // idx_a = [1,4,3,-1,-1,5,0,-1,2]
+        // idx_a is the inverse of a. indx_a[a[i]] = i;
+        // the distance (width) from a[i] to a[j] is found by
+        // [1, 4, 3, -1, -1, 5, 0, -1, 2]
+        //  0, 1, 2,  3,  4, 5, 6, 7,  8
+        //
+        // distance from 0 -> 1 is indx_a[1] - indx_a[0] etc.
         int maxWidthRamp(std::vector<int>& a)
         {
             auto res = 0;
