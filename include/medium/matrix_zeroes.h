e4389d428b02a52a8bbea99763271d70b8e169f7
Joe Berria <joeberria@gmail.com>
2021-02-09 18:45:42 -0800

    matrix zero space O(1) per constraint

--- include/medium/matrix_zeroes.h
+++ include/medium/matrix_zeroes.h
@@ -59,5 +59,47 @@ namespace Leet::Medium {
                 }
             }
         }
+
+        // space O(1)
+        void zetZeroes_constant_space(std::vector<std::vector<int>>& matrix)
+        {
+            int row_size = matrix.size();
+            int col_size = matrix[0].size();
+            bool zero_col = false;
+            bool zero_row = false;
+
+            // when we find a zero, mark the first cell in that row and col as
+            // zero
+            for (int row = 0; row < row_size; row++) {
+                for (int col = 0; col < col_size; col++) {
+                    if (matrix[row][col] == 0) {
+                        if (row == 0) zero_row = true;
+                        if (col == 0) zero_col = true;
+
+                        matrix[row][0] = 0;
+                        matrix[0][col] = 0;
+                    }
+                }
+            }
+
+            // start at [1,1] and if the starting cell in that row and col is zero
+            // set [row, col] to zero
+            for (int row = 1; row < row_size; row++) {
+                for (int col = 1; col < col_size; col++) {
+                    if (matrix[row][0] == 0 || matrix[0][col] == 0)
+                        matrix[row][col] = 0;
+                }
+            }
+
+            // clear first row if needed
+            if (zero_row)
+                for (int i = 0; i < col_size; i++)
+                    matrix[0][i] = 0;
+
+            // clear first column if needed
+            if (zero_col)
+                for (int i = 0; i < row_size; i++)
+                    matrix[i][0] = 0;
+        }
     };
 }  // namespace Leet::Medium
