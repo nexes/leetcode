d20216c62245ff90affbb66ac3a1e1ff5d90aac1
Joe Berria <joeberria@gmail.com>
2021-03-27 11:43:39 -0700

    formatting

--- include/medium/find_peak.h
+++ include/medium/find_peak.h
@@ -2,23 +2,24 @@
 
 #include <vector>
 
-namespace Leet::Medium
-{
+namespace Leet::Medium {
     // A peak element is an element that is greater than its neighbors.
-    // Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
-    // The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
-    // You may imagine that nums[-1] = nums[n] = -∞.
+    // Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and
+    // return its index. The array may contain multiple peaks, in that case return the
+    // index to any one of the peaks is fine. You may imagine that nums[-1] = nums[n] =
+    // -∞.
 
     // Example 1:
     // Input: nums = [1,2,3,1]
     // Output: 2
-    // Explanation: 3 is a peak element and your function should return the index number 2.
+    // Explanation: 3 is a peak element and your function should return the index
+    // number 2.
 
     // Example 2:
     // Input: nums = [1,2,1,3,5,6,4]
     // Output: 1 or 5
-    // Explanation: Your function can return either index number 1 where the peak element is 2,
-    // or index number 5 where the peak element is 6.
+    // Explanation: Your function can return either index number 1 where the peak element
+    // is 2, or index number 5 where the peak element is 6.
 
     // Follow up: Your solution should be in logarithmic complexity.
     struct FindPeak
@@ -38,8 +39,7 @@ namespace Leet::Medium
             int right = nums.size() - 1;
             int mid;
 
-            while (left < right)
-            {
+            while (left < right) {
                 mid = (right + left) / 2;
 
                 if (nums[mid] > nums[mid + 1])
@@ -51,4 +51,4 @@ namespace Leet::Medium
             return left;
         }
     };
-} // namespace Leet::Medium
+}  // namespace Leet::Medium
