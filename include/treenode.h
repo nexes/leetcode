26686d7422812e810aab5fece3af38a9cbebe8bc
Joe Berria <joeberria@gmail.com>
2021-04-26 11:30:26 -0700

    formatting common functions

--- include/treenode.h
+++ include/treenode.h
@@ -4,12 +4,21 @@ namespace Leet {
     // Definition for a binary tree from leetcode problems
     struct TreeNode
     {
+        TreeNode() : val(0), left(nullptr), right(nullptr)
+        {
+        }
+
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr)
+        {
+        }
+
+        TreeNode(int x, TreeNode *left, TreeNode *right)
+            : val(x), left(left), right(right)
+        {
+        }
+
         int val;
         TreeNode *left;
         TreeNode *right;
-
-        TreeNode() : val(0), left(nullptr), right(nullptr) {}
-        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
     };
 }  // namespace Leet
