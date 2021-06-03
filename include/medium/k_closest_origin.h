20fc28dbc0a02d17e70ff7f7eb39c862d1c9dda7
Joe Berria <joeberria@gmail.com>
2021-06-03 12:06:47 -0700

    formatting

--- include/medium/k_closest_origin.h
+++ include/medium/k_closest_origin.h
@@ -6,8 +6,8 @@
 #include <vector>
 
 namespace Leet::Medium {
-    // We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
-    // (Here, the distance between two points on a plane is the Euclidean distance.)
+    // We have a list of points on the plane.  Find the K closest points to the origin (0,
+    // 0). (Here, the distance between two points on a plane is the Euclidean distance.)
     // You may return the answer in any order.  The answer is guaranteed to be unique
     // (except for the order that it is in.)
 
@@ -18,7 +18,8 @@ namespace Leet::Medium {
     // The distance between (1, 3) and the origin is sqrt(10).
     // The distance between (-2, 2) and the origin is sqrt(8).
     // Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
-    // We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
+    // We only want the closest K = 1 points from the origin, so the answer is just
+    // [[-2,2]].
 
     // Example 2:
     // Input: points = [[3,3],[5,-1],[-2,4]], K = 2
@@ -31,7 +32,8 @@ namespace Leet::Medium {
     // -10000 < points[i][1] < 10000
     struct KClosestPoint
     {
-        std::vector<std::vector<int>> kClosest(std::vector<std::vector<int>>& points, int k)
+        std::vector<std::vector<int>> kClosest(std::vector<std::vector<int>>& points,
+                                               int k)
         {
             auto results = std::vector<std::vector<int>>{};
             auto dist_map = std::map<double, std::vector<std::vector<int>>>{};
@@ -43,7 +45,7 @@ namespace Leet::Medium {
                 };
 
                 if (auto it = dist_map.emplace(dist, point_bucket); !it.second) {
-                    //collision
+                    // collision
                     it.first->second.emplace_back(point);
                 }
             }
@@ -61,16 +63,18 @@ namespace Leet::Medium {
         }
 
         // cleaner but slower
-        std::vector<std::vector<int>> kClosest_cleaner(std::vector<std::vector<int>>& points, int k)
+        std::vector<std::vector<int>> kClosest_cleaner(
+            std::vector<std::vector<int>>& points, int k)
         {
             auto results = std::vector<std::vector<int>>{};
 
-            std::sort(points.begin(), points.end(), [](const std::vector<int> a, const std::vector<int> b) {
-                double dist_a = std::sqrt((a[0] * a[0]) + (a[1] * a[1]));
-                double dist_b = std::sqrt((b[0] * b[0]) + (b[1] * b[1]));
+            std::sort(points.begin(), points.end(),
+                      [](const std::vector<int> a, const std::vector<int> b) {
+                          double dist_a = std::sqrt((a[0] * a[0]) + (a[1] * a[1]));
+                          double dist_b = std::sqrt((b[0] * b[0]) + (b[1] * b[1]));
 
-                return dist_a < dist_b;
-            });
+                          return dist_a < dist_b;
+                      });
 
             results.insert(results.end(), points.begin(), points.begin() + k);
             return results;
