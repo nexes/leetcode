2b8d19b5ac7dc6c01a574bb5f8be088266fa7887
Joe Berria <joeberria@gmail.com>
2021-07-09 18:49:35 -0700

    formatting

--- include/medium/find_peak.h
+++ include/medium/find_peak.h
@@ -6,8 +6,7 @@ namespace Leet::Medium {
     // A peak element is an element that is greater than its neighbors.
     // Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and
     // return its index. The array may contain multiple peaks, in that case return the
-    // index to any one of the peaks is fine. You may imagine that nums[-1] = nums[n] =
-    // -∞.
+    // index to any one of the peaks is fine. You may imagine that nums[-1] = nums[n] = -∞.
 
     // Example 1:
     // Input: nums = [1,2,3,1]
