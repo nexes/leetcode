e45593c6a493e01dd5dfe3ede530c3b03c900784
Joe Berria <joeberria@gmail.com>
2021-10-16 12:28:10 -0700

    formatting

--- include/medium/rotated_array.h
+++ include/medium/rotated_array.h
@@ -2,15 +2,13 @@
 
 #include <vector>
 
-namespace Leet::Medium
-{
+namespace Leet::Medium {
+    // Suppose an array sorted in ascending order is rotated at some pivot unknown to you
+    // beforehand. (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
 
-    // Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
-    // (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
-
-    // You are given a target value to search. If found in the array return its index, otherwise return -1.
-    // You may assume no duplicate exists in the array.
-    // Your algorithm's runtime complexity must be in the order of O(log n).
+    // You are given a target value to search. If found in the array return its index,
+    // otherwise return -1. You may assume no duplicate exists in the array. Your
+    // algorithm's runtime complexity must be in the order of O(log n).
 
     // Example 1:
     // Input: nums = [4,5,6,7,0,1,2], target = 0
@@ -28,26 +26,22 @@ namespace Leet::Medium
             int r = len - 1;
             int mid = (r + l) / 2;
 
-            while (l <= r)
-            {
+            while (l <= r) {
                 mid = (r + l) / 2;
 
                 if (nums[mid] == target)
                     return mid;
 
-                // a sorted rotated array, when taken the mid point will have either the right
-                // or the left side sorted. Both if the pivot happens to be the mid point.
-                // start with the sorted side, that way we can see if our target is on the right or the
-                // left
-                if (nums[mid] >= nums[l])
-                {
+                // a sorted rotated array, when taken the mid point will have either the
+                // right or the left side sorted. Both if the pivot happens to be the mid
+                // point. start with the sorted side, that way we can see if our target is
+                // on the right or the left
+                if (nums[mid] >= nums[l]) {
                     if (target > nums[mid] || target < nums[l])
                         l = mid + 1;
                     else
                         r = mid - 1;
-                }
-                else
-                {
+                } else {
                     if (target < nums[mid] || target > nums[r])
                         r = mid - 1;
                     else
@@ -58,4 +52,4 @@ namespace Leet::Medium
             return -1;
         }
     };
-} // namespace Leet::Medium
+}  // namespace Leet::Medium
