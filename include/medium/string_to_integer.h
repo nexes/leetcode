c1b08b2b49edd8bd1320fccf29f2a728b4b4d58d
Joe Berria <joeberria@gmail.com>
2022-01-09 16:04:15 -0800

    string to integer update

--- include/medium/string_to_integer.h
+++ include/medium/string_to_integer.h
@@ -1,27 +1,28 @@
 #pragma once
 
-#include <string>
 #include <algorithm>
+#include <string>
 
-namespace Leet::Medium
-{
+namespace Leet::Medium {
     // Implement atoi which converts a string to an integer.
-    // The function first discards as many whitespace characters as necessary until
-    // the first non-whitespace character is found. Then, starting from this character,
-    // takes an optional initial plus or minus sign followed by as many numerical digits
-    // as possible, and interprets them as a numerical value. The string can contain
-    // additional characters after those that form the integral number, which are ignored
-    // and have no effect on the behavior of this function. If the first sequence of
-    // non-whitespace characters in str is not a valid integral number, or if no such
-    // sequence exists because either str is empty or it contains only whitespace
-    // characters, no conversion is performed. If no valid conversion could be performed,
-    // a zero value is returned.
+    // The function first discards as many whitespace characters as necessary
+    // until the first non-whitespace character is found. Then, starting from
+    // this character, takes an optional initial plus or minus sign followed by
+    // as many numerical digits as possible, and interprets them as a numerical
+    // value. The string can contain additional characters after those that form
+    // the integral number, which are ignored and have no effect on the behavior
+    // of this function. If the first sequence of non-whitespace characters in
+    // str is not a valid integral number, or if no such sequence exists because
+    // either str is empty or it contains only whitespace characters, no
+    // conversion is performed. If no valid conversion could be performed, a
+    // zero value is returned.
 
     // Note:
     // Only the space character ' ' is considered as whitespace character.
-    // Assume we are dealing with an environment which could only store integers within the 32-bit
-    // signed integer range: [−231,  231 − 1]. If the numerical value is out of the range of representable
-    // values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
+    // Assume we are dealing with an environment which could only store integers
+    // within the 32-bit signed integer range: [−231,  231 − 1]. If the
+    // numerical value is out of the range of representable values, INT_MAX (231
+    // − 1) or INT_MIN (−231) is returned.
 
     // Example 1:
     // Input: "42"
@@ -30,25 +31,28 @@ namespace Leet::Medium
     // Example 2:
     // Input: "   -42"
     // Output: -42
-    // Explanation: The first non-whitespace character is '-', which is the minus sign.
-    //              Then take as many numerical digits as possible, which gets 42.
+    // Explanation: The first non-whitespace character is '-', which is the
+    // minus sign. Then take as many numerical digits as possible, which
+    // gets 42.
 
     // Example 3:
     // Input: "4193 with words"
     // Output: 4193
-    // Explanation: Conversion stops at digit '3' as the next character is not a numerical digit.
+    // Explanation: Conversion stops at digit '3' as the next character is not a
+    // numerical digit.
 
     // Example 4:
     // Input: "words and 987"
     // Output: 0
-    // Explanation: The first non-whitespace character is 'w', which is not a numerical
-    //              digit or a +/- sign. Therefore no valid conversion could be performed.
+    // Explanation: The first non-whitespace character is 'w', which is not a
+    // numerical digit or a +/- sign. Therefore no valid conversion could be
+    // performed.
 
     // Example 5:
     // Input: "-91283472332"
     // Output: -2147483648
-    // Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
-    //              Thefore INT_MIN (−231) is returned.
+    // Explanation: The number "-91283472332" is out of the range of a 32-bit
+    // signed integer. Thefore INT_MIN (−231) is returned.
 
     struct Atoi
     {
@@ -61,21 +65,32 @@ namespace Leet::Medium
             int sign = 1;
 
             // remove whitespace
-            if (s.length() > 0)
-                s.erase(std::remove_if(s.begin(), s.end(), [](unsigned char c) { return std::isspace(c); }), s.end());
+            std::stringstream ss(s);
+            ss >> s;
+
+            // remove whitespace
+            // if (s.length() > 0)
+            //     s.erase(std::remove_if(s.begin(), s.end(),
+            //                            [](unsigned char c) {
+            //                                return std::isspace(c);
+            //                            }),
+            //             s.end());
 
             if (s.empty())
                 return 0;
 
-            if (s.at(0) == '+' || s.at(0) == '-')
-            {
+            if (s.at(0) == '+' || s.at(0) == '-') {
                 i = 1;
                 if (s.at(0) == '-')
                     sign = -1;
             }
 
-            while (i < s.length() && isdigit(s.at(i)))
+            while (i < s.length() && isdigit(s.at(i))) {
                 x = x * 10 + s.at(i++) - '0';
+
+                if (x > max)
+                    break;
+            }
 
             x *= sign;
             x = std::max(std::min(x, max), min);
@@ -83,4 +98,4 @@ namespace Leet::Medium
             return x;
         }
     };
-} // namespace Leet::Medium
+}  // namespace Leet::Medium
