317a8f1bee7b2a86228d5dffabfa8588877eeeb2
Joe Berria <joeberria@gmail.com>
2023-02-19 00:53:50 -0800

    formatting

--- include/medium/array_nesting.h
+++ include/medium/array_nesting.h
@@ -5,13 +5,14 @@
 #include <vector>
 
 namespace Leet::Medium {
-    // A zero-indexed array A of length N contains all integers from 0 to N-1. Find and
-    // return the longest length of set S, where S[i] = {A[i], A[A[i]], A[A[A[i]]], ... }
-    // subjected to the rule below.
+    // A zero-indexed array A of length N contains all integers from 0 to N-1.
+    // Find and return the longest length of set S, where S[i] = {A[i], A[A[i]],
+    // A[A[A[i]]], ... } subjected to the rule below.
 
-    // Suppose the first element in S starts with the selection of element A[i] of index =
-    // i, the next element in S should be A[A[i]], and then A[A[A[i]]]… By that analogy,
-    // we stop adding right before a duplicate element occurs in S.
+    // Suppose the first element in S starts with the selection of element A[i]
+    // of index = i, the next element in S should be A[A[i]], and then
+    // A[A[A[i]]]… By that analogy, we stop adding right before a duplicate
+    // element occurs in S.
 
     // Example 1:
     // Input: A = [5,4,0,3,1,6,2]
