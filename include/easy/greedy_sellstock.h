9c8dee13a8bce1687948065dc0ac8d2d11079913
Joe Berria <joeberria@gmail.com>
2023-07-03 11:44:40 -0700

    cleaner solution

--- include/easy/greedy_sellstock.h
+++ include/easy/greedy_sellstock.h
@@ -1,27 +1,32 @@
 #pragma once
 
+#include <algorithm>
 #include <vector>
 
-namespace Leet::Easy
-{
-    // Say you have an array prices for which the ith element is the price of a given stock on day i.
-    // Design an algorithm to find the maximum profit. You may complete as many transactions
-    // as you like (i.e., buy one and sell one share of the stock multiple times).
-    // Note: You may not engage in multiple transactions at the same time
-    // (i.e., you must sell the stock before you buy again).
+namespace Leet::Easy {
+    // Say you have an array prices for which the ith element is the price of a
+    // given stock on day i. Design an algorithm to find the maximum profit. You
+    // may complete as many transactions as you like (i.e., buy one and sell one
+    // share of the stock multiple times). Note: You may not engage in multiple
+    // transactions at the same time (i.e., you must sell the stock before you
+    // buy again).
 
     // Example 1:
     // Input: [7,1,5,3,6,4]
     // Output: 7
-    // Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
-    //              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+    // Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5),
+    // profit = 5-1 = 4.
+    //              Then buy on day 4 (price = 3) and sell on day 5 (price = 6),
+    //              profit = 6-3 = 3.
 
     // Example 2:
     // Input: [1,2,3,4,5]
     // Output: 4
-    // Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
-    //              Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
-    //              engaging multiple transactions at the same time. You must sell before buying again.
+    // Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5),
+    // profit = 5-1 = 4.
+    //              Note that you cannot buy on day 1, buy on day 2 and sell
+    //              them later, as you are engaging multiple transactions at the
+    //              same time. You must sell before buying again.
 
     // Example 3:
     // Input: [7,6,4,3,1]
@@ -29,20 +34,38 @@ namespace Leet::Easy
     // Explanation: In this case, no transaction is done, i.e. max profit = 0.
     struct SellStock
     {
+        // cleaner way
         int maxProfit(std::vector<int> &prices)
+        {
+            int buy = prices[0];
+            int sell = prices[0];
+            int profit = sell - buy;
+
+            for (const int price : prices) {
+                if (price < buy) {
+                    buy = price;
+                    sell = price;
+                }
+
+                if (price > sell) {
+                    sell = price;
+                    profit = std::max(profit, sell - buy);
+                }
+            }
+
+            return profit;
+        }
+
+        int maxProfit_old(std::vector<int> &prices)
         {
             int size = prices.size();
             int buy_idx = -1;
             int profit = 0;
 
-            for (int i = 0; i < size - 1; i++)
-            {
-                if (prices[i + 1] > prices[i] && buy_idx < 0)
-                {
+            for (int i = 0; i < size - 1; i++) {
+                if (prices[i + 1] > prices[i] && buy_idx < 0) {
                     buy_idx = i;
-                }
-                else if (prices[i + 1] < prices[i] && buy_idx >= 0)
-                {
+                } else if (prices[i + 1] < prices[i] && buy_idx >= 0) {
                     profit += prices[i] - prices[buy_idx];
                     buy_idx = -1;
                 }
@@ -54,4 +77,4 @@ namespace Leet::Easy
             return profit;
         }
     };
-} // namespace Leet::Easy
+}  // namespace Leet::Easy
