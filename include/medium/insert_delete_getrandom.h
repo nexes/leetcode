63bf65edfc9f1e4a6fc8c628dfa976b04bbe2c97
Joe Berria <joeberria@gmail.com>
2023-07-05 12:20:00 -0700

    insert delete random medium problem

---
+++ include/medium/insert_delete_getrandom.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <cstdlib>
+#include <vector>
+#include <unordered_map>
+
+namespace Leet::Medium {
+    // Implement the RandomizedSet class:
+
+    // RandomizedSet() Initializes the RandomizedSet object.
+    // bool insert(int val) Inserts an item val into the set if not present.
+    // Returns true if the item was not present, false otherwise. bool
+    // remove(int val) Removes an item val from the set if present. Returns true
+    // if the item was present, false otherwise. int getRandom() Returns a
+    // random element from the current set of elements (it's guaranteed that at
+    // least one element exists when this method is called). Each element must
+    // have the same probability of being returned. You must implement the
+    // functions of the class such that each function works in average O(1) time
+    // complexity.
+
+    // Example 1:
+    // Input
+    // ["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove",
+    // "insert", "getRandom"]
+    // [[], [1], [2], [2], [], [1], [2], []]
+    // Output
+    // [null, true, false, true, 2, true, false, 2]
+    // Explanation:
+    // RandomizedSet randomizedSet = new RandomizedSet();
+    // randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was
+    // inserted successfully. randomizedSet.remove(2); // Returns false as 2
+    // does not exist in the set. randomizedSet.insert(2); // Inserts 2 to the
+    // set, returns true. Set now contains [1,2]. randomizedSet.getRandom(); //
+    // getRandom() should return either 1 or 2 randomly.
+    // randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now
+    // contains [2]. randomizedSet.insert(2); // 2 was already in the set, so
+    // return false. randomizedSet.getRandom(); // Since 2 is the only number in
+    // the set, getRandom() will always return 2.
+
+    // Constraints:
+    // -231 <= val <= 231 - 1
+    // At most 2 * 105 calls will be made to insert, remove, and getRandom.
+    // There will be at least one element in the data structure when getRandom
+    // is called.
+    struct GetRandom
+    {
+    public:
+        GetRandom() : list(), map()
+        {
+        }
+
+        bool insert(int val)
+        {
+            if (auto elem = map.find(val); elem == map.end()) {
+                list.push_back(val);
+                map.insert({val, list.size() - 1});
+                return true;
+            }
+
+            return false;
+        }
+
+        bool remove(int val)
+        {
+            if (auto removed = map.find(val); removed != map.end()) {
+                int last_elem = list.back();
+
+                list[removed->second] = last_elem;
+                map[last_elem] = removed->second;
+
+                list.pop_back();
+                map.erase(val);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        int getRandom()
+        {
+            int idx = std::rand() % list.size();
+            return list[idx];
+        }
+
+    private:
+        std::vector<int> list;
+        std::unordered_map<int, int> map;
+    };
+}  // namespace Leet::Medium
