edfa22e6bbb73458ae1e7fe91bd5f96addcf3a6e
Joe Berria <joeberria@gmail.com>
2023-07-13 10:36:37 -0700

    cleaner solution

--- include/easy/length_last_word.h
+++ include/easy/length_last_word.h
@@ -3,11 +3,13 @@
 #include <string>
 
 namespace Leet::Easy {
-    // Given a string s consists of upper/lower-case alphabets and empty space characters ' ',
-    // return the length of last word (last word means the last appearing word if we loop from
-    // left to right) in the string. If the last word does not exist, return 0.
+    // Given a string s consists of upper/lower-case alphabets and empty space
+    // characters ' ', return the length of last word (last word means the last
+    // appearing word if we loop from left to right) in the string. If the last
+    // word does not exist, return 0.
 
-    // Note: A word is defined as a maximal substring consisting of non-space characters only.
+    // Note: A word is defined as a maximal substring consisting of non-space
+    // characters only.
 
     // Example:
     // Input: "Hello World"
@@ -29,6 +31,20 @@ namespace Leet::Easy {
             }
 
             return count;
+        }
+
+        int lengthOfLastWord_cleaner(std::string s)
+        {
+            int len = 0;
+            int idx = s.length() - 1;
+
+            while (idx >= 0 && s[idx] == ' ')
+                idx--;
+
+            while (idx >= 0 && s[idx--] != ' ')
+                len++;
+
+            return len;
         }
     };
 }  // namespace Leet::Easy
