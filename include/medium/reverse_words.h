41b8c51296464de932d6a18af03fc0af52e04249
Joe Berria <joeberria@gmail.com>
2023-07-15 08:53:11 -0700

    cleaner solution

--- include/medium/reverse_words.h
+++ include/medium/reverse_words.h
@@ -3,6 +3,7 @@
 #include <../common.h>
 
 #include <algorithm>
+#include <sstream>
 #include <string>
 
 namespace Leet::Medium {
@@ -15,23 +16,42 @@ namespace Leet::Medium {
     // Example 2:
     // Input: "  hello world!  "
     // Output: "world! hello"
-    // Explanation: Your reversed string should not contain leading or trailing spaces.
+    // Explanation: Your reversed string should not contain leading or trailing
+    // spaces.
 
     // Example 3:
     // Input: "a good   example"
     // Output: "example good a"
-    // Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+    // Explanation: You need to reduce multiple spaces between two words to a
+    // single space in the reversed string.
 
     // Note:
     // A word is defined as a sequence of non-space characters.
-    // Input string may contain leading or trailing spaces. However, your reversed string should
-    // not contain leading or trailing spaces. You need to reduce multiple spaces between
-    // two words to a single space in the reversed string.
+    // Input string may contain leading or trailing spaces. However, your
+    // reversed string should not contain leading or trailing spaces. You need
+    // to reduce multiple spaces between two words to a single space in the
+    // reversed string.
     struct ReverseWords
     {
-        // honestly I'm just having fun writing the common string functions found in include/common.h
-        // this was more about writing the trim, split and reverse functions than solving this problem :)
         std::string reverseWords(std::string s)
+        {
+            std::string rev = "";
+            std::string word = "";
+
+            std::istringstream ss(s);
+
+            while (ss >> word)
+                rev = word + " " + rev;
+
+            // remove the trailing space
+            rev.pop_back();
+
+            return rev;
+        }
+        // honestly I'm just having fun writing the common string functions
+        // found in include/common.h this was more about writing the trim, split
+        // and reverse functions than solving this problem :)
+        std::string reverseWords_playing_around(std::string s)
         {
             std::string return_str = "";
 
@@ -41,8 +61,7 @@ namespace Leet::Medium {
             auto sv = algo.split(s);
             algo.reverse(sv);
 
-            for (auto &word : sv)
-            {
+            for (auto &word : sv) {
                 if (return_str.length() > 0)
                     return_str += " ";
                 return_str += word;
