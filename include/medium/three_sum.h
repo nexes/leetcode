ce5e039c58601207c382dba0f8b45adbe8bd44d6
Joe Berria <joeberria@gmail.com>
2024-07-25 12:30:34 -0700

    refactor three sum

--- include/medium/three_sum.h
+++ include/medium/three_sum.h
@@ -5,9 +5,8 @@
 
 namespace Leet::Medium {
     // Given an array nums of n integers, are there elements a, b, c in nums such
-    // that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
-    // Note:
-    // The solution set must not contain duplicate triplets.
+    // that a + b + c = 0? Find all unique triplets in the array which gives the sum of
+    // zero. Note: The solution set must not contain duplicate triplets.
 
     // Example:
     // Given array nums = [-1, 0, 1, 2, -1, -4],
@@ -18,6 +17,8 @@ namespace Leet::Medium {
     // ]
     struct ThreeSum
     {
+        // time: O(n^2)
+        // space: O(1)
         std::vector<std::vector<int>> threeSum(std::vector<int> &nums)
         {
             std::vector<std::vector<int>> result;
@@ -28,26 +29,29 @@ namespace Leet::Medium {
             std::sort(nums.begin(), nums.end());
 
             for (int i = 0; i < len - 1; i++) {
+                // skip duplicates from i
                 if (i > 0 && nums[i] == nums[i - 1])
                     continue;
 
                 l = i + 1;
                 r = len - 1;
-
                 while (l < r) {
-                    if (nums[i] + nums[l] + nums[r] == 0) {
-                        result.emplace_back(std::vector{nums[i], nums[l], nums[r]});
-                        while (l < r && nums[l] == nums[l + 1])
-                            l++;
-                        while (l < r && nums[r] == nums[r - 1])
-                            r--;
+                    int check = nums[i] + nums[l] + nums[r];
+
+                    if (check == 0) {
+                        result.push_back({nums[i], nums[l], nums[r]});
                         l++;
                         r--;
-                    } else {
-                        if (nums[i] + nums[l] + nums[r] < 0)
+
+                        // skip duplicates for l and r
+                        while (l < r && nums[l] == nums[l - 1])
                             l++;
-                        else
+                        while (l < r && nums[r] == nums[r + 1])
                             r--;
+                    } else if (check < 0) {
+                        l++;
+                    } else {
+                        r--;
                     }
                 }
             }
