47726dcc9216b0783964dfc9509171f98f9a457c
Joe Berria <joeberria@gmail.com>
2024-08-14 14:46:00 -0700

    intersecting arrays rewrite

--- include/easy/intersecting_array.h
+++ include/easy/intersecting_array.h
@@ -4,7 +4,10 @@
 #include <vector>
 
 namespace Leet::Easy {
-    // Given two arrays, write a function to compute their intersection.
+    // Given two integer arrays nums1 and nums2, return an array of their
+    // intersection
+    // . Each element in the result must be unique and you may return the result in any
+    // order.
 
     // Example 1:
     // Input: nums1 = [1,2,2,1], nums2 = [2,2]
@@ -13,26 +16,31 @@ namespace Leet::Easy {
     // Example 2:
     // Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
     // Output: [9,4]
+    // Explanation: [4,9] is also accepted.
 
-    // Note:
-    // Each element in the result must be unique.
-    // The result can be in any order.
+    // Constraints:
+    // 1 <= nums1.length, nums2.length <= 1000
+    // 0 <= nums1[i], nums2[i] <= 1000
     struct IntersectArray
     {
+        // time: O(n + m)
+        // space: O(n)
         std::vector<int> intersection(std::vector<int> nums1, std::vector<int> nums2)
         {
-            std::unordered_set<int> p(nums1.begin(), nums1.end());
-            std::unordered_set<int> q(nums2.begin(), nums2.end());
-            std::vector<int> out;
+            std::unordered_set<int> numset;
+            std::vector<int> result;
 
-            // O(n)
-            for (auto &i : q) {
-                // O(1)
-                if (p.find(i) != p.end())
-                    out.emplace_back(i);
+            for (int num : nums1)
+                numset.insert(num);
+
+            for (int i = 0; i < nums2.size(); i++) {
+                if (numset.count(nums2[i]) == 1) {
+                    result.push_back(nums2[i]);
+                    numset.erase(nums2[i]);
+                }
             }
 
-            return out;
+            return result;
         }
     };
 }  // namespace Leet::Easy
