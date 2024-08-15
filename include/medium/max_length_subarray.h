b4d1b2287d4374ba1b1d82f5ed35baec0edf8170
Joe Berria <joeberria@gmail.com>
2024-08-14 18:39:48 -0700

    refactor longest common subarray

--- include/medium/max_length_subarray.h
+++ include/medium/max_length_subarray.h
@@ -2,48 +2,47 @@
 
 #include <vector>
 
+using std::vector;
+
 namespace Leet::Medium {
-    // Given two integer arrays A and B, return the maximum length of an
-    // subarray that appears in both arrays.
+    // Given two integer arrays nums1 and nums2, return the maximum length of a subarray
+    // that appears in both arrays.
 
     // Example 1:
-    // Input:
-    // A: [1,2,3,2,1]
-    // B: [3,2,1,4,7]
+    // Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
     // Output: 3
+    // Explanation: The repeated subarray with maximum length is [3,2,1].
 
-    // Explanation:
-    // The repeated subarray with maximum length is [3, 2, 1].
+    // Example 2:
+    // Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+    // Output: 5
+    // Explanation: The repeated subarray with maximum length is [0,0,0,0,0].
 
-    // Note:
-    // 1 <= len(A), len(B) <= 1000
-    // 0 <= A[i], B[i] < 100
+    // Constraints:
+
+    // 1 <= nums1.length, nums2.length <= 1000
+    // 0 <= nums1[i], nums2[i] <= 100
     struct MaxSubArray
     {
-        int findLength(std::vector<int>& a, std::vector<int>& b)
+        // O(n^2)
+        int findLength(std::vector<int>& nums1, std::vector<int>& nums2)
         {
-            auto dp = std::vector<std::vector<int>>(
-                a.size() + 1, std::vector<int>(b.size() + 1));
-            int max = 0;
+            int maxlen = 0;
+            vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0));
 
-            for (int i = 0; i <= a.size(); i++)
-                dp[i][0] = 0;
-
-            for (int i = 0; i <= b.size(); i++)
-                dp[0][i] = 0;
-
-            for (int i = 1; i <= a.size(); i++) {
-                for (int j = 1; j <= b.size(); j++) {
-                    if (a[i - 1] == b[j - 1])
-                        dp[i][j] = 1 + dp[i - 1][j - 1];
+            // classic DP, longest common substring (subarray)
+            for (int r = 1; r <= nums1.size(); r++) {
+                for (int c = 1; c <= nums2.size(); c++) {
+                    if (nums1[r - 1] == nums2[c - 1])
+                        dp[r][c] = dp[r - 1][c - 1] + 1;
                     else
-                        dp[i][j] = 0;
+                        dp[r][c] = 0;
 
-                    max = std::max(max, dp[i][j]);
+                    maxlen = std::max(maxlen, dp[r][c]);
                 }
             }
 
-            return max;
+            return maxlen;
         }
     };
 }  // namespace Leet::Medium
