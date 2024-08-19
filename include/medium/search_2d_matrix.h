ab1043089bd4dfacd0ddc9de39207cd562a2bb3e
Joe Berria <joeberria@gmail.com>
2024-08-19 10:41:37 -0700

    search 2d matrix refactor

--- include/medium/search_2d_matrix.h
+++ include/medium/search_2d_matrix.h
@@ -3,11 +3,13 @@
 #include <vector>
 
 namespace Leet::Medium {
-    // Write an efficient algorithm that searches for a value in an m x n matrix.
-    // This matrix has the following properties:
-    //   Integers in each row are sorted from left to right.
-    //   The first integer of each row is greater than the last integer of the
-    //   previous row.
+    // You are given an m x n integer matrix matrix with the following two properties:
+
+    // Each row is sorted in non-decreasing order.
+    // The first integer of each row is greater than the last integer of the previous row.
+    // Given an integer target, return true if target is in matrix or false otherwise.
+
+    // You must write a solution in O(log(m * n)) time complexity.
 
     // Example 1:
     // Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
@@ -24,8 +26,48 @@ namespace Leet::Medium {
     // -104 <= matrix[i][j], target <= 104
     struct Search2D
     {
-        // time: O(nlog(n)), space: O(1)
+        // O(log(m*n))
         bool searchMatrix(std::vector<std::vector<int>>& matrix, int target)
+        {
+            int m = matrix.size();
+            int n = matrix[0].size();
+            int row = 0;
+
+            // binary search find the row
+            int l = 0;
+            int r = m - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+
+                if (matrix[mid][0] <= target && matrix[mid][n - 1] >= target) {
+                    row = mid;
+                    break;
+                }
+                if (target < matrix[mid][0])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+
+            // binary search the row
+            l = 0;
+            r = n - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+
+                if (matrix[row][mid] == target)
+                    return true;
+                if (matrix[row][mid] < target)
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+
+            return false;
+        }
+
+        // time: O(n + log(m)), space: O(1)
+        bool searchMatrix_linear(std::vector<std::vector<int>>& matrix, int target)
         {
             int rows = matrix.size();
 
@@ -40,7 +82,7 @@ namespace Leet::Medium {
             return false;
         }
 
-        // O(log(n))
+        // O(log(m))
         bool search(std::vector<int>& row, int target)
         {
             int low = 0;
