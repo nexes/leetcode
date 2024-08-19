3bc5bf63a69723137188c9923405bc90f26489ea
Joe Berria <joeberria@gmail.com>
2024-08-19 16:04:33 -0700

    update to linked list cycle easy problem

--- include/easy/linked_list_cycle_1.h
+++ include/easy/linked_list_cycle_1.h
@@ -4,19 +4,21 @@
 
 namespace Leet::Easy {
     // Given a linked list, determine if it has a cycle in it.
-    // To represent a cycle in the given linked list, we use an integer pos which represents the
-    // position (0-indexed) in the linked list where tail connects to. If pos is -1, then there
-    // is no cycle in the linked list.
+    // To represent a cycle in the given linked list, we use an integer pos which
+    // represents the position (0-indexed) in the linked list where tail connects to. If
+    // pos is -1, then there is no cycle in the linked list.
 
     // Example 1:
     // Input: head = [3,2,0,-4], pos = 1
     // Output: true
-    // Explanation: There is a cycle in the linked list, where tail connects to the second node.
+    // Explanation: There is a cycle in the linked list, where tail connects to the second
+    // node.
 
     // Example 2:
     // Input: head = [1,2], pos = 0
     // Output: true
-    // Explanation: There is a cycle in the linked list, where tail connects to the first node.
+    // Explanation: There is a cycle in the linked list, where tail connects to the first
+    // node.
 
     // Example 3:
     // Input: head = [1], pos = -1
@@ -32,16 +34,15 @@ namespace Leet::Easy {
             Leet::ListNode *slow = root;
             Leet::ListNode *fast = root;
 
-            if (!root || !root->next) return false;
-
-            while (fast && fast->next) {
+            while (fast != nullptr && fast->next != nullptr) {
                 slow = slow->next;
                 fast = fast->next->next;
 
-                if (slow == fast) break;
+                if (slow == fast)
+                    return true;
             }
 
-            return slow == fast;
+            return false;
         }
     };
 }  // namespace Leet::Easy
