14350e418ba41cd5b3b7bd8c945544ca8da4eee1
Joe Berria <joeberria@gmail.com>
2024-09-20 17:25:09 -0700

    longest palindrom substr refactor

--- include/medium/palindrome_substr.h
+++ include/medium/palindrome_substr.h
@@ -20,38 +20,37 @@ namespace Leet::Medium {
         // Constraints:
         // 1 <= s.length <= 1000
         // s consist of only digits and English letters.
+        int palindromLength(std::string s, int l, int r)
+        {
+            while (l >= 0 && r < s.length() && s[l] == s[r]) {
+                l--;
+                r++;
+            }
+
+            return r - l - 1;
+        }
+
         std::string longestPalindrome(std::string s)
         {
-            int len = 0;
             int start = 0;
-            std::string palindrome = "";
-
-            auto check = [&](int l, int r) -> int {
-                while (l >= 0 && r < s.length() && s[l] == s[r]) {
-                    l--;
-                    r++;
-                }
-                return r - l - 1;
-            };
+            int len = 0;
 
             for (int i = 0; i < s.length(); i++) {
-                // check for even and odd length strings
-                int even = check(i, i);
-                int odd = check(i, i + 1);
-
-                if (even > len) {
-                    len = even;
-                    start = i - (len / 2);
-                }
+                int odd = palindromLength(s, i, i);
+                int even = palindromLength(s, i, i + 1);
 
                 if (odd > len) {
                     len = odd;
+                    start = i - (len / 2);
+                }
+
+                if (even > len) {
+                    len = even;
                     start = i - (len / 2) + 1;
                 }
             }
 
-            palindrome = s.substr(start, len);
-            return palindrome;
+            return s.substr(start, len);
         }
     };
 }  // namespace Leet::Medium
