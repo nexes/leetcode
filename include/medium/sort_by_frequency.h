05ad2d0bdc1ee0d42a880883c7671af2c59d8cc1
Joe Berria <joeberria@gmail.com>
2024-09-27 23:09:49 -0700

    sort by frequency update

--- include/medium/sort_by_frequency.h
+++ include/medium/sort_by_frequency.h
@@ -1,10 +1,8 @@
 #pragma once
 
-#include <algorithm>
 #include <queue>
 #include <string>
 #include <unordered_map>
-#include <vector>
 
 namespace Leet::Medium {
     // Given a string, sort it in decreasing order based on the frequency of
@@ -36,67 +34,26 @@ namespace Leet::Medium {
         // O(nlog(n))
         std::string frequencySort(std::string s)
         {
-            std::string out{""};
-            auto freq_map = std::unordered_map<char, int>{};
-            auto freq_str = std::vector<std::string>{};
+            std::string sortedStr = "";
+            std::unordered_map<char, int> freqMap;
+            std::priority_queue<std::pair<int, char>> freqQueue;
 
             // O(n)
-            for (const auto& c : s) {
-                if (auto it = freq_map.insert({c, 1}); !it.second) {
-                    freq_map[c]++;
-                }
-            }
-
-            // O(n) roughly
-            for (const auto& m : freq_map) {
-                std::string temp{""};
-
-                for (int i = 0; i < m.second; i++)
-                    temp.push_back(m.first);
-
-                freq_str.emplace_back(temp);
-            }
+            for (char c : s)
+                freqMap[c]++;
 
             // O(nlog(n))
-            std::sort(freq_str.begin(), freq_str.end(),
-                      [](const std::string& a, const std::string& b) {
-                          return a.length() > b.length();
-                      });
+            for (auto [c, count] : freqMap)
+                freqQueue.push({count, c});
 
-            // O(n)
-            for (auto& s : freq_str)
-                out.append(s);
-
-            return out;
-        }
-
-        // priority queue
-        std::string frequencySort_queue(std::string s)
-        {
-            std::string out{""};
-            auto freq_map = std::unordered_map<char, std::string>{};
-            auto freq_queue =
-                std::priority_queue<std::pair<int, std::string>>{};
-
-            for (const auto& c : s) {
-                if (auto it = freq_map.emplace(c, std::string{c}); !it.second) {
-                    freq_map[c].push_back(c);
-                }
+            // O(nlog(n))
+            while (!freqQueue.empty()) {
+                auto [count, c] = freqQueue.top();
+                freqQueue.pop();
+                sortedStr.append(count, c);
             }
 
-            for (const auto [k, v] : freq_map) {
-                std::cout << "inserting " << v << "\n";
-                freq_queue.emplace(v.length(), v);
-            }
-
-            while (!freq_queue.empty()) {
-                auto str = freq_queue.top();
-                freq_queue.pop();
-
-                out.append(str.second);
-            }
-
-            return out;
+            return sortedStr;
         }
     };
 }  // namespace Leet::Medium
