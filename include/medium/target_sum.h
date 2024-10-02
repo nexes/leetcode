853d485d7801593e0e495306392bb367c385d70c
Joe Berria <joeberria@gmail.com>
2024-10-02 09:36:04 -0700

    target sum top down dp

--- include/medium/target_sum.h
+++ include/medium/target_sum.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 
+using std::vector;
+
 namespace Leet::Medium {
     // You are given a list of non-negative integers, a1, a2, ..., an, and a target, S.
     // Now you have 2 symbols + and -. For each integer, you should choose one from
@@ -28,6 +30,36 @@ namespace Leet::Medium {
 
     struct TargetSum
     {
+        // ------------------------ TOP DOWN DP ----------------------
+        int findTarget_topdown(vector<int>& nums, vector<vector<int>>& memo, int total,
+                               int target, int current, int idx)
+        {
+            if (idx == nums.size() && current == target)
+                return 1;
+            if (idx == nums.size())
+                return 0;
+
+            if (memo[idx][current + total] != -1)
+                return memo[idx][current + total];
+
+            int add = findTarget_topdown(nums, memo, total, target, current + nums[idx],
+                                         idx + 1);
+            int sub = findTarget_topdown(nums, memo, total, target, current - nums[idx],
+                                         idx + 1);
+
+            return memo[idx][current + total] = add + sub;
+        }
+
+        int findTargetSumWays_topdown(vector<int>& nums, int target)
+        {
+            int totalSum = 0;
+            for (int num : nums)
+                totalSum += num;
+
+            vector<vector<int>> memo(nums.size(), vector<int>(totalSum * 2 + 1, -1));
+            return findTarget_topdown(nums, memo, totalSum, target, 0, 0);
+        }
+
         // brute force DFS
         int findTargetSumWays(std::vector<int>& nums, int s)
         {
